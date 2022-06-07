e6fb8f874234a19d0f316f6c317cff8a63224ce5
Thomas Berube-lambert <tberube-@c2r2p10.42quebec.com>
2022-06-07 14:50:49 -0400

    pipex start comment

--- pipex_start.c
+++ pipex_start.c
@@ -6,7 +6,7 @@
 /*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 11:17:02 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/07 12:57:55 by tberube-         ###   ########.fr       */
+/*   Updated: 2022/06/07 14:49:54 by tberube-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,37 @@ void	pipex_start(int argc, char **argv, char **envp, t_struct *data)
 {
 	pipe(data->pipefd);
 	data->child = fork();
-	if (data->child = 0)
+	if (data->child = 0) //dans le child
 	{
-		dup2(data->fds[0], 0);
-		dup2(data->pipefd[1], 1);
-		
+		dup2(data->fds[0], 0); // remplace l'entrée standard par ton fichier
+		//close()
+		dup2(data->pipefd[1], 1); // remplace la sortie standard par la sortie de ton pipe
+		// close
+		// EXECUTE TA MARDE
+		// EXIT(1)
 	}
+	return (pipe[0]) //doit lire dans le end car le pipe vas continuer vers la 2e cmd
 }
-
-// void	open_fd(char **argv, t_struct *data)
+// void	pipex_end(int argc, char **argv, char **envp, t_struct *data)
 // {
-// 	data->fds[0] = open(argv[1], O_RDONLY);
-// 	data->fds[1] = open(argv[4], O_RDWR, 0644);
-// 	if (data->fds[0] < 0 || data->fds[1] < 0)
-// 		quit_cmd(data);
+//
+// 	data->child = fork();
+// 	if (data->child = 0) //dans le child
+// 	{
+// 		dup2(data->fd_IN DE LA DERNIERE FOONCYTION, 0); // remplace l'entrée standard par ton fichier
+// 		//close()
+// 		dup2(data->TA SORTIE, 1); // remplace la sortie standard par la sortie de ton pipe
+// 		// close
+		//EXECUTE TA MARDE
+		//QUIT SI ERREUR
+		
+// 	}
+// 	return (pipe[0])
 // }
+void	open_fd(char **argv, t_struct *data)
+{
+	data->fds[0] = open(argv[1], O_RDONLY);
+	data->fds[1] = open(argv[4], O_RDWR, 0644);
+	if (data->fds[0] < 0 || data->fds[1] < 0)
+		quit_cmd(data);
+}
