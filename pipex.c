7373e1ac6a9748566638c6d95741c15db535c165
Thomas Berube-lambert <tberube-@c2r1p6.42quebec.com>
2022-06-15 15:44:54 -0400

    printf enlever

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 10:56:23 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/15 15:12:55 by tberube-         ###   ########.fr       */
+/*   Updated: 2022/06/15 15:37:14 by tberube-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ int	main(int argc, char **argv, char **envp)
 	parsing(argc, argv, envp, &data); 	// premier fd (argv1) existe access existe ou existe pas ou fopen return 
 	open_fd(argv, &data);				// premiere cmd (argv2) existe ou pas, avec envp
 						 				// deuxieme cmd (argv3) existe ou pas, avec envp
-	//printf("de n'importe quoi\n");									// deuxieme fd (argv4) existe ou pas, si existe pas tu le creer
+										// deuxieme fd (argv4) existe ou pas, si existe pas tu le creer
 	pipex_start(argv, envp, &data);
-	
 	pipex_end(argv, envp, &data);
 	quit_cmd(&data);
 }
--- pipex_start.c
+++ pipex_start.c
@@ -6,7 +6,7 @@
 /*   By: tberube- <tberube-@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 11:17:02 by tberube-          #+#    #+#             */
-/*   Updated: 2022/06/15 15:31:06 by tberube-         ###   ########.fr       */
+/*   Updated: 2022/06/15 15:39:49 by tberube-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,12 @@
 void	pipex_start(char **argv, char **envp, t_struct *data)
 {
 	if (pipe(data->pipefd) < 0)
-	{
-	//	printf("pipefd");
 		return ;	
-	}
 	if (data->fds[0] != -1)
 	{
-	//	printf("a l'exterieur du 1er child : \n");
 		data->child = fork();
 		if (data->child == 0)
 		{
-			//printf("a l'inteieur du 1er child : \n");
 			dup2(data->fds[0], 0);
 			close(data->fds[0]);
 			dup2(data->pipefd[1], 1);
@@ -34,9 +29,7 @@ void	pipex_start(char **argv, char **envp, t_struct *data)
 			execve(data->cmd_path[0], ft_split(argv[2], ' '), envp);
 			free(data->cmd_path[0]);
 			exit(1);
-	//		printf("child exit");
 		}	
-	//	dprintf(2, "else child 1\n");
 		//close(data->fds[0]);
 		dup2(data->pipefd[0], 0);
 		close(data->pipefd[0]);
@@ -49,14 +42,11 @@ void	pipex_start(char **argv, char **envp, t_struct *data)
 
 void	pipex_end(char **argv, char **envp, t_struct *data)
 {
-	//dprintf(2, "criss\n");
 	if (data->fds[1] != -1)
 	{
 		data->child = fork();
-		//dprintf(2, "a l'exterieur du 2e child : %d\n", data->child);
 		if (data->child == 0)
 		{
-			//printf("a l'interieur du 2e child : %d\n", data->child);
 			//close(data->fds[1]);
 			execve(data->cmd_path[1], ft_split(argv[3], ' '), envp);
 			dprintf(2, "child 2 error");
